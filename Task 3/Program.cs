0857f6fe0fe0dfad591667c46e4c86c47b61e44d
Denis <info.strakhovka@mail.ru>
2022-11-24 12:09:32 +0300

    Задача на кратность 7 и 23

---
+++ Task 3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Кратно ли число одновременно 7 и 23?");
+Console.WriteLine("Введите число больше 23 ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 ==0 && a % 23 == 0)
+    Console.WriteLine("Отлично! Кратно! ");
+else
+    Console.WriteLine("Не кратно! Попробуй еще раз!");
