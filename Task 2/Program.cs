a1b62e941ea8ca22cc1cdfb256aa741d1db85e7e
Denis <info.strakhovka@mail.ru>
2022-11-24 11:53:58 +0300

    Add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите 2 целых числа");
+int a = Convert.ToInt32(Console.ReadLine ());
+int b = Convert.ToInt32(Console.ReadLine ());
+if (a % b == 0)
+Console.WriteLine("Кратно! Отлично!");
+else
+Console.WriteLine($"Не кратно! Остаток = {a % b}");
